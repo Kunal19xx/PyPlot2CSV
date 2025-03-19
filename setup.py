d6c5c3c8e950334935966673dd0936bb599a71d9
Kunal19xx <88104802+Kunal19xx@users.noreply.github.com>
2025-03-19 15:58:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pyplot2csv",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "pandas", "scipy", "sklearn"],
+    install_requires=["numpy", "matplotlib", "pandas", "scipy", "scikit-learn"],
     entry_points={
         "console_scripts": [
             "pyplot2csv=pyplot2csv.cli:main",
